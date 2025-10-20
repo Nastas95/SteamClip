2c23e91bde3a7cbe82b55aa4e45de9b86710257f
Nastas95 <Trentiniema@gmail.com>
2025-10-20 18:25:53 +0200

    New v3.0 Base Code

--- steamclip.py
+++ steamclip.py
@@ -164,7 +164,7 @@ class SteamClipApp(QWidget):
     CONFIG_FILE = os.path.join(CONFIG_DIR, 'SteamClip.conf')
     GAME_IDS_FILE = os.path.join(CONFIG_DIR, 'GameIDs.json')
     STEAM_APP_DETAILS_URL = "https://store.steampowered.com/api/appdetails"
-    CURRENT_VERSION = "v2.19"
+    CURRENT_VERSION = "v3.0"
 
     def __init__(self):
         super().__init__()
